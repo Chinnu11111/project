a837a9a40bd1d42ab79820b60203bdb5cba9be2e
chinnu11111 <chinnu6045@gmail.com>
2023-05-31 18:28:04 -0400

    final commit

--- fibo.py
+++ fibo.py
@@ -1,14 +1,11 @@
 def fibonacci(n):
-    fib_seq = [0, 1]  # Initialize the sequence with the first two numbers
+    fib_seq = [0, 1] 
     
-    # Generate the Fibonacci sequence up to the desired length
     while len(fib_seq) < n:
-        next_num = fib_seq[-1] + fib_seq[-2]  # Compute the next number in the sequence
-        fib_seq.append(next_num)  # Add the next number to the sequence
-        
+        next_num = fib_seq[-1] + fib_seq[-2]  
+        fib_seq.append(next_num)  
     return fib_seq
 
-# Example usage
 sequence_length = 10
 fib_numbers = fibonacci(sequence_length)
 print(fib_numbers)
